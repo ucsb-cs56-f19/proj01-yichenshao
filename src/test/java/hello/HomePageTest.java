ea6b9091220264a4f5c971097920fae184994d37
Yichen <yichenshao@umail.ucsb.edu>
2019-11-18 17:59:54 -0800

    YS - write a failing test and make the test pass for page 1

--- src/test/java/hello/HomePageTest.java
+++ src/test/java/hello/HomePageTest.java
@@ -70,4 +70,12 @@ public class HomePageTest {
                 .andExpect(xpath("/html/body/div/nav/a").string("lab07"));
     }
 
+    @Test
+    public void getPageOne_hasCorrectName() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.TEXT_HTML))
+                .andExpect(status().isOk())
+	        .andExpect(xpath("/html/body/div/nav/div/ul[1]/li[2]/a").exists())
+                .andExpect(xpath("/html/body/div/nav/div/ul[1]/li[2]/a").string("Earthquakes"));
+    }
+    
 }
