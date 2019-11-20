404cf3f0577a2146e68e6cadc5b774dda7df3994
Yichen <yichenshao@umail.ucsb.edu>
2019-11-20 01:23:42 -0800

    YS - made id clickable

--- src/main/java/hello/geojson/Feature.java
+++ src/main/java/hello/geojson/Feature.java
@@ -4,6 +4,6 @@ public class Feature {
   public String type;
   public Properties properties;
   public String id;
-  public String url;
+    //  public String url;
     //public Metadata metadata;
 }
