9bc9c3b6a58136c175641bc5eef1c8011a2d1e16
yichenshao <yichenshao@umail.ucsb.edu>
2019-11-20 02:27:59 -0800

    Revert "Ys java objects"

--- src/main/java/hello/geojson/Feature.java
+++
@@ -1,9 +0,0 @@
-package hello.geojson;
-
-public class Feature {
-  public String type;
-  public Properties properties;
-  public String id;
-    //  public String url;
-    //public Metadata metadata;
-}
