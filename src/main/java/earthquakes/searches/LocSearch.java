b1eb6d53faa27a0aca078a12cc3de41c4fc9b787
Yichen <yichenshao@umail.ucsb.edu>
2019-12-02 23:39:49 -0800

    YS - added location search

---
+++ src/main/java/earthquakes/searches/LocSearch.java
@@ -0,0 +1,19 @@
+package earthquakes.searches;
+
+public class LocSearch {
+
+    private String location;
+
+    public LocSearch(){
+	location = "";
+    }
+
+    public void setLocation(String location){
+	this.location = location;
+    }
+
+    public String getLocation(){
+	return location;
+    }
+    
+}
