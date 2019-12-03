4ce7bdd09c540cf458701b26d4941e7290a061b1
Yichen <yichenshao@umail.ucsb.edu>
2019-12-02 21:56:31 -0800

    YS - replaced every hello with earthquakes and compiled and tested

--- src/test/java/earthquakes/HomePageTest.java
+++ src/test/java/earthquakes/HomePageTest.java
@@ -1,4 +1,4 @@
-package hello;
+package earthquakes;
 
 // import static org.hamcrest.Matchers.equalTo;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -17,10 +17,10 @@ import org.springframework.security.oauth2.client.registration.ClientRegistratio
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.xpath;
-
+import earthquakes.controllers.HomeController;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(WebController.class)
+@WebMvcTest(HomeController.class)
 public class HomePageTest {
 
     @Autowired
