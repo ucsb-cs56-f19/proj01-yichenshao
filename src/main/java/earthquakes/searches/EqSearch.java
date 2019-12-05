845bf8fc5d5bf45a08c9a15c2445ccc1efe04635
Yichen <yichenshao@umail.ucsb.edu>
2019-12-04 18:15:41 -0800

    YS - added fields to bean and forms

--- src/main/java/earthquakes/searches/EqSearch.java
+++ src/main/java/earthquakes/searches/EqSearch.java
@@ -3,11 +3,20 @@ package earthquakes.searches;
 public class EqSearch{
     private int distance;
     private int minmag;
+    private double lat;
+    private double lon;
+    private String location;
 
     public int getDistance(){return this.distance;}
     public int getMinmag(){return this.minmag;}
+    public double getLat(){return this.lat;}
+    public double getLon(){return this.lon;}
+    public Srting getLocation(){return this.location;}
 
     public void setDistance(int a){this.distance = a;}
     public void setMinmag(int a){this.minmag = a;}
+    public void setLat(double a){this.lat = a;}
+    public void setLon(double a){this.lon = a;}
+    public void setLocation(String a){this.location = a;}
 }
     
